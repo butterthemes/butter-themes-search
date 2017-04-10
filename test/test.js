a6779033a1be7d7ccf65d0e094ec61f2165f8ce2
Baltazar Gomez <btzr.io@gmail.com>
2017-04-10 16:16:42 -0600

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,11 +1,21 @@
-const honeyButter = require('../index.js');
+const butterThemes = require('../index.js');
 
-console.log(" @honeyButter is searching... \n");
+console.log(" Searching themes... \n");
 
-honeyButter.search((themes) => {
-    console.log(" @honeyButter found: " + themes.length + " themes.");
-    themes.map( (item, index) => {
+butterThemes.search((results) => {
+    let themes = [],
+        official = [],
+        thirdParty = [];
+    //Get search stats...
+    resuts.map((item, index) => {
         let verify = (item.official) ? "official" : "third-party";
-        console.log(" --- butter-theme("+ verify +"):",item.name, item.version, "["+index+"]");
+        let data = " --- butter-theme(" + verify + "): " + item.name + " v:"item.version + " [" + index + "]";
+        (item.official) ? official.push(index) : thirdParty.push(index);
+        themes.push(data);
+        
     });
+    //Print stats ...
+    console.log(" Results found:", official.length + " official,", thirdparty.length + " third-party,", themes.length + " total.");
+    //Print results...
+    results.map((theme) => console.log(theme));
 });
